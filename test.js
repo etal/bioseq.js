7179d47999e466a15216c1a3bd587d970ad1e456
Eric Talevich <eric.talevich@gmail.com>
2012-07-01 23:36:07 -0400

    Added a test script

---
+++ test.js
@@ -0,0 +1,33 @@
+bs = require("./bioseq");
+
+// Functions on raw strings
+rawseq = "GATTACATG";
+
+rnaseq = bs.transcribe(rawseq);
+bs.backTranscribe(rnaseq);
+
+bs.complement(rawseq);
+bs.reverseComplement(rawseq);
+bs.letterCounts(rawseq);
+bs.letterFrequencies(rawseq);
+bs.translate(rawseq);
+bs.translate3frames(rawseq);
+bs.translate6frames(rawseq);
+bs.ungap(rawseq);
+
+// Methods on the Sequence object
+myseq = bs.readFasta(">myseq hey hey hey\n" + rawseq)[0];
+
+myrnaseq = myseq.transcribe();
+myrnaseq.backTranscribe();
+
+// myseq.complement();
+myseq.letterCounts();
+myseq.letterFrequencies();
+myseq.reverseComplement();
+myseq.translate();
+myseq.translate3frames();
+myseq.translate6frames();
+myseq.ungap();
+
+console.log(bs.toFasta(myseq));
