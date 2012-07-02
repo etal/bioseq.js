47522399d1503104c74041aab5379b376fd97bc2
Eric Talevich <eric.talevich@gmail.com>
2012-07-01 23:31:53 -0400

    Fixed a small but crucial bug in reverseComplement

--- bioseq.js
+++ bioseq.js
@@ -70,7 +70,7 @@ module.exports.reverseComplement = reverseComplement = function (seq) {
     // ENH: warn about non-DNA characters
     var out = [];
     // Traverse the string in reverse
-    for (var i = seq.length - 1; i != 0; i--) {
+    for (var i = seq.length - 1; i >= 0; i--) {
         // Swap base pairs
         out.push(DnaComplements[seq[i]]);
     }
